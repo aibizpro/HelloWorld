2adef9bb0b55b5067c0b65ed90759ef63480b239
marcingawle <marcingawle@gmail.com>
2024-12-06 14:12:42 +0100

    tresci zadan i blod

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,4 +3,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Witaj świecie!");
   }
-}
+//przy próbie uruchomienia tej klasy dostaję komunikat:
+//java: reached end of file while parsing
+//D:\IdeaProjects\HelloWorld\src\HelloWorld.java:5:4
